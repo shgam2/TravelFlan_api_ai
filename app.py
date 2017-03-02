84fb759dd3a80d0df40e7970995c41e80a2be780
Thomas Yook <thomas.yook@gmail.com>
2017-03-02 00:25:47 -0800

    working

--- app.py
+++ app.py
@@ -97,15 +97,12 @@ def process_request(req):
 
         post_fields = {'text': str(phrase), "to": str(code)}
         print('call 1 started')
-        print(TRANSLATE_BASE_URL)
-        requests.post('http://awseb-e-f-AWSEBLoa-VIW6OYVV6CSY-1979702995.us-east-1.elb.amazonaws.com/translate',
+
+        response = requests.post('http://awseb-e-f-AWSEBLoa-VIW6OYVV6CSY-1979702995.us-east-1.elb.amazonaws.com/translate',
                       json.dumps({'to': 'ja', 'text': '111'}), headers={'Content-Type': 'application/json',
                                                                         'Authorization': 'Basic dHJhdmVsZmxhbjp0b3VyMTIzNA=='})
-        print('call 1 ended')
-        print('call 2 started')
-        response = requests.post(TRANSLATE_BASE_URL, json.dumps({'to': 'ja', 'text': 'i love you'}), headers={'Content-Type': 'application/json', 'Authorization': 'Basic dHJhdmVsZmxhbjp0b3VyMTIzNA=='})
+        #response = requests.post(TRANSLATE_BASE_URL, json.dumps({'to': 'ja', 'text': 'i love you'}), headers={'Content-Type': 'application/json', 'Authorization': 'Basic dHJhdmVsZmxhbjp0b3VyMTIzNA=='})
 
-        print('call 2 ended')
         print(json.dumps(response, indent=4))
         speech = '"%s" in %s is "%s"' % (phrase, language, response.body)
         res = {
