6396f7906465fe62da12bc4ca83c1411d762ad5f
shgam2 <hey_w_s@hotmail.com>
2017-03-03 16:46:53 +0900

    ..

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ dir_file_en = 'transportation_en.csv'
 dir_file_cn = 'transportation_cn.csv'
 dir_file_tw = 'transportation_tw.csv'
 
-
 def make_yql_query(req):
     city = req['result']['parameters']['geo-city']
     return 'select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\'%s\')' % (city,)
@@ -39,7 +38,6 @@ def find_language_code(lang):
         'chinese simplified': 'zh-cn',
         'chinese traditional': 'zh-tw',
     }.get(lang)
-
 
 def process_request(req):
     res = None
@@ -80,8 +78,8 @@ def process_request(req):
         }
     elif action == 'direction':
         speech = parse_json(req)
-        print("111111111111")
-        print(speech)
+        print ("111111111111")
+        print (speech)
         res = {
             'speech': speech,
             'displayText': speech,
@@ -98,7 +96,7 @@ def process_request(req):
         print(language)
         print(code)
 
-        url = TRANSLATE_BASE_URL + urlencode({'text': phrase, 'to': code, 'authtoken': 'dHJhdmVsZmxhbjp0b3VyMTIzNA=='})
+        url = TRANSLATE_BASE_URL + urlencode({'text': phrase, 'to': code, 'authtoken': 'dHJhdmVsZmxhbjp0b3VyMTIzNA==' })
         print(url)
         _res = urlopen(url).read()
         print('im here1')
@@ -112,19 +110,18 @@ def process_request(req):
         }
     return res
 
-
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
     print('Request:\n%s' % (json.dumps(req, indent=4),))
 
     res = process_request(req)
-    print("RESSS")
-    print(res)
+    print ("RESSS")
+    print (res)
     try:
         res = json.dumps(res, indent=4)
     except Exception as e:
-        print(e)
+        print (e)
     print('Response:\n%s' % (res,))
 
     r = make_response(res)
@@ -139,11 +136,11 @@ def parse_json(req):
     # print("----------------req --------------------")
     # print(req)
     # print("----------------req --------------------")
-    # if req.get['originalRequest'] is None:
+    #if req.get['originalRequest'] is None:
     #    dir_file = dir_file_en
-    # else:
+    #else:
     lang_code = req['originalRequest']['data'].get('locale')
-    # print ("**************lang_code = {}".format(lang_code))
+    #print ("**************lang_code = {}".format(lang_code))
     if lang_code == "zh_TW" or lang_code == "zh_HK":
         # use traditional chinese
         dir_file = dir_file_tw
@@ -153,6 +150,7 @@ def parse_json(req):
     else:
         # use english
         dir_file = dir_file_en
+
 
     result = req.get("result")
     parameters = result.get("parameters")
@@ -181,43 +179,51 @@ def grab_answer(loc1, loc2, dir_file):
     print("in grab_answer function")
     print("filename = {}".format(dir_file))
 
-    with open(dir_file, 'rU') as f:
-        direction = list(unicodecsv.reader(f, encoding='utf-8'))
+    try:
+        with open(dir_file, 'rU') as f:
+            print('START')
+            print(f.read())
+            direction = list(csv.reader(f))
+            print('END')
 
-        from_loc = loc1
-        to_loc = loc2
-        row_num = 0
-        col_num = 0
-        count = 0
+            from_loc = loc1
+            to_loc = loc2
+            row_num = 0
+            col_num = 0
+            count = 0
 
-        print("from_loc = {}".format(from_loc))
-        print("to_loc = {}".format(to_loc))
+            print("from_loc = {}".format(from_loc))
+            print("to_loc = {}".format(to_loc))
 
-        while True:
-            print("inside loop now!!")
-            if direction[count][0] == "":
-                # location not found
-                break
-            if direction[count][0] == from_loc:
-                row_num = count
-                count = 0
-                print("rownum = {}".format(row_num))
-                break
-            count = count + 1
+            while True:
+                print("inside loop now!!")
+                if direction[count][0] == "":
+                    # location not found
+                    break
+                if direction[count][0] == from_loc:
+                    row_num = count
+                    count = 0
+                    print("rownum = {}".format(row_num))
+                    break
+                count = count + 1
 
-        while True:
-            if direction[0][count] == "":
-                # location not found
-                break
-            if direction[0][count] == to_loc:
-                col_num = count
-                count = 0
-                print("colnum = {}".format(col_num))
-                break
-            count = count + 1
-        print(direction[row_num][col_num])
-        speech = direction[row_num][col_num]
-        return speech
+            while True:
+                if direction[0][count] == "":
+                    # location not found
+                    break
+                if direction[0][count] == to_loc:
+                    col_num = count
+                    count = 0
+                    print("colnum = {}".format(col_num))
+                    break
+                count = count + 1
+            print(direction[row_num][col_num])
+            speech = direction[row_num][col_num]
+            return speech
+    except IOError:
+        print("exception error")
+    except Exception as e:
+        print("something weird happened", e)
 
 
 if __name__ == '__main__':
