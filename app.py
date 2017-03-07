9cf9eb6e7ce43a7ccc47a3d2365d088337a82bef
shgam2 <hey_w_s@hotmail.com>
2017-03-08 05:00:56 +0900

    weather-testing

--- app.py
+++ app.py
@@ -346,13 +346,14 @@ def process_request(req):
                     print (fc_weather)
 
                     if userlocale.lower() == "zh_cn" or userlocale.lower() == "zh_tw" or userlocale.lower() == "zh_hk":
-                        # print("333333333333")
-                        # print("1. "+fc_weather['date'])
-                        # print("2. " +conv_weather_cond(fc_weather['text']))
-                        # print("3. " +)
-                        # print("4. " +)
-                        #print("1:{}  2:{}".format(fc_weather['date'], conv_weather_cond(fc_weather['text'])))
                         cond = conv_weather_cond(fc_weather['text'])
+                        print("333333333333")
+                        print("1. "+fc_weather['date'])
+                        print("2. " +cond)
+                        print("3. " +fc_weather['high'])
+                        print("4. " +units['temperature'])
+                        #print("1:{}  2:{}".format(fc_weather['date'], conv_weather_cond(fc_weather['text'])))
+
                         speech += '\n(%s) %s, 高溫: %s°%s, 低溫: %s°%s' % (
                             fc_weather['date'], cond, fc_weather['high'],
                             units['temperature'], fc_weather['low'], units['temperature'])
