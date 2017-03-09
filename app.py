6da840d854f08c8a5504812bf4e730b8123d0eb1
Seok Lim <limseok@limseok-MacBookPro.local>
2017-03-09 15:49:19 +0900

    restaurant test...

--- app.py
+++ app.py
@@ -326,11 +326,14 @@ def exapi_pengtai(data):
     #     'longitude': None,
     #     'distance': None
     # }
+    print('data')
     timestamp = str(int(time.time()))
     plain = '%s|%s' % (PENGTAI_KEY, timestamp)
     headers = {'X-hash': hashlib.sha256(plain.encode('utf-8')).hexdigest()}
 
     data['timestamp'] = timestamp
+
+    print('TEST!!')
 
     try:
         res = requests.get(PENGTAI_TEST_URL, headers=headers, params=data)
@@ -488,8 +491,6 @@ def process_request(req):
             lang = '04'
             button_title = 'Click to view'
 
-        print(1, userlocale)
-
         cuisine = req['result']['parameters']['cuisine'].lower()
         if cuisine == 'korean':
             category2 = '3101'
@@ -510,15 +511,10 @@ def process_request(req):
         else:
             category2 = None
 
-        print(2, cuisine)
-        print(3, category2)
-
         address = req['result']['parameters']['address']
         geocode_result = gmaps.geocode(address)
         latitude = geocode_result[0]['geometry']['location']['lat']
         longitude = geocode_result[0]['geometry']['location']['lng']
-
-        print('TEST4', latitude, longitude)
 
         _data = {
             'lang': lang,
@@ -531,6 +527,8 @@ def process_request(req):
             'distance': '500'
         }
         _res = exapi_pengtai(_data)
+
+        print('TEST!', _res)
 
         speech = 'None'
 
