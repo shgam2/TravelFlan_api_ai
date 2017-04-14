ac8d2d47fe0731f869202c3b60cd63d1341fad60
shgam2 <hey_w_s@hotmail.com>
2017-04-14 22:18:36 +0900

    Brian: restaurant update (Friday Apr 14)

--- app.py
+++ app.py
@@ -1447,6 +1447,46 @@ def process_request(req):
                 print('1. GURUNAVI')
                 _res = exapi_gurunavi(_data)
 
+                elements = list()
+                if not _res['rest']:
+                    print("Empty list!")
+                else:
+                    print("HERERERERE")
+                    for i, item in enumerate(_res['rest']):
+                        fb_item = {
+                            'title': item['name']['name'],
+                            'subtitle': '%s\n%s' % (item['name']['name_sub'], item['contacts']['address']),
+                            'image_url': item['image_url']['thumbnail'],
+                            'buttons': [
+                                {
+                                    'type': 'web_url',
+                                    'url': item['url'],
+                                    'title': 'TEMP BUTTON TITLE'
+                                }
+                            ]
+                        }
+                        elements.append(fb_item)
+                        if userlocale == 'zh_cn':
+                            speech += '%s. 名称: %s\n簡介: %s\n地址: %s\n連絡電話: %s\n營業時間: %s\n\n' % (
+                                i + 1, item['name']['name'], item['name']['name_sub'], item['contacts']['address'],
+                                item['contacts']['tel'], item['business_hour']
+                            )
+                        elif userlocale in ('zh_tw', 'zh_hk'):
+                            speech += '%s. 名稱: %s\n簡介: %s\n地址: %s\n連絡電話: %s\n營業時間: %s\n\n' % (
+                                i + 1, item['name']['name'], item['name']['name_sub'], item['contacts']['address'],
+                                item['contacts']['tel'], item['business_hour']
+                            )
+                        else:
+                            speech += '%s. Name: %s\nSummary: %s\nAddress: %s\nTel: %s\nBusiness hours: %s\n\n' % (
+                                i + 1, item['name']['name'], item['name']['name_sub'], item['contacts']['address'],
+                                item['contacts']['tel'], item['business_hour']
+                            )
+
+                        speech += '%s. name: %s\nsummary: %s\naddress: %s\ntel: %s\nbusiness hours: %s\n\n' % (
+                            i + 1, item['name']['name'], item['name']['name_sub'], item['contacts']['address'],
+                            item['contacts']['tel'], item['business_hour']
+                        )
+
             elif country == 'korea':
                 _data = {
                     'lang': lang,
