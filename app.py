36898534311d04196e15ed9a29b44dddd5a6c8ea
shgam2 <hey_w_s@hotmail.com>
2017-04-16 21:15:32 +0900

    Brian: restaurant update (Friday Apr 14)

--- app.py
+++ app.py
@@ -1510,7 +1510,8 @@ def process_request(req):
                                  'Please try with different parameters.'
                     else:
                         for i, item in enumerate(_res['rest']):
-                            print('HEREEEEEEEEEEEEEE: {}'.format(item['name']['name']))
+                            print('HEREEEEEEEEEEEEEE---------------------: \nHEREEEEEEEEEEEEEE---------------------\n'
+                                  '{}'.format(item))
                             fb_item = {
                                 'title': item['name']['name'],
                                 'subtitle': '%s\n%s' % (item['access'], item['contacts']['address']),
